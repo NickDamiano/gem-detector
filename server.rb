29640c3c4cb3f5dd648b238bc413071a661abb15
Nick Damiano <damiano.nick@gmail.com>
2015-01-22 23:50:53 -0600

    adds gemlist.erb and a post method grabbing params

--- server.rb
+++ server.rb
@@ -3,8 +3,13 @@ require 'sinatra'
 set :bind, '0.0.0.0' #vagrant stuff
 
 post '/gemfile' do 
-  
+  puts params
+  erb :gemlist, :locals => {test: params['gemfile']}
 end
+
+# get '/gemfile' do 
+
+# end
 
 get '/' do 
   erb :index
