fe28ef3dbfb349c353bc67f935d9aba3ecbde9c7
Nick Damiano <damiano.nick@gmail.com>
2015-01-24 21:41:12 -0600

    changes post route to pass the form variable directly to the result page

--- server.rb
+++ server.rb
@@ -3,8 +3,7 @@ require 'sinatra'
 set :bind, '0.0.0.0' #vagrant stuff
 
 post '/gemfile' do 
-  gems = params['gemfile']
-  erb :result, :locals => {gemlist: gems}
+  
 end
 
 get '/gemfile' do 
--- test.rb
+++ test.rb
@@ -3,14 +3,11 @@ require 'sinatra'
 set :bind, '0.0.0.0' #vagrant stuff
 
 post '/gemfile' do 
-  gems = params['gemfile']
-  erb :result, :locals => {gemlist: gems}
 end
 
 get '/gemfile' do 
-  
 end
 
 get '/' do 
-  erb :index
+  erb :test
 end
