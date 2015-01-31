0be5c267997bc512e9311a0c0bec3bd8b371f0e2
Nick Damiano <damiano.nick@gmail.com>
2015-01-30 23:36:27 -0600

    Removes console logs from app.rb, parse gems, and lookup gems. Adds TODOs to parse_gems to fix the error where the gems indented in the gemfile are missed.

--- lib/gemfile_lookup/scripts/lookup_gems.rb
+++ lib/gemfile_lookup/scripts/lookup_gems.rb
@@ -1,19 +1,13 @@
 require 'open-uri'
 require 'json'
-#TODO 
+
 #gets an array of hashes with gem name and version number or possibly just a sub array. 
 #returns an array of hashes with all of the different information about each gem
-#api call gets gem information
-#gem information is stored in hash
-#hash is pushed into array
-#repeated
 
 module GemfileLookup
   class RubyGemsCall
     def self.run(gem_list)
       api_result = []
-      p 'GEM LIST BELOW'
-      p gem_list
       gem_list.each do |gem_name|
         gem_info = open("https://rubygems.org/api/v1/gems/#{gem_name}.json").read
         gem_info_parsed = JSON.parse(gem_info)
--- web/app.rb
+++ web/app.rb
@@ -8,8 +8,6 @@ post '/gemfile' do
   gems = params['gemfile']
   result = GemfileLookup::Parse.run(gems)
   api_return = GemfileLookup::RubyGemsCall.run(result[:gem_list])
-  p 'api return!!!!!!!!!!!!'
-  p api_return[:result]
   erb :result, :locals => {gemlist: api_return[:result]}
 end
 
