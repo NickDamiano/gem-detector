6225f942a0c5159c34bb8af237ea4ca0c0dfd64b
Nick Damiano <damiano.nick@gmail.com>
2015-02-20 23:18:10 -0600

    adds commented code referencing threads.

--- lib/gemfile_lookup/scripts/lookup_gems.rb
+++ lib/gemfile_lookup/scripts/lookup_gems.rb
@@ -9,18 +9,37 @@ module GemfileLookup
   class RubyGemsCall
     def self.run(gem_list)
       api_result = []
+
       gem_list.each do |gem_name|
-        begin
-          gem_info = open("https://rubygems.org/api/v1/gems/#{gem_name}.json").read
-          rescue OpenURI::HTTPError 
-            api_result.push({"name" => gem_name, "info" => "Gem not found at rubygems.org"})
+          begin
+            gem_info = open("https://rubygems.org/api/v1/gems/#{gem_name}.json").read
+            rescue OpenURI::HTTPError 
+              api_result.push({"name" => gem_name, "info" => "Gem not found at rubygems.org"})
+          end
+            gem_info_parsed = JSON.parse(gem_info) unless gem_info.nil?
+            api_result.push(gem_info_parsed) unless gem_info_parsed.nil?
         end
-          gem_info_parsed = JSON.parse(gem_info) unless gem_info.nil?
-          api_result.push(gem_info_parsed) unless gem_info_parsed.nil?
-
-
-      end
       return { success?: true, result: api_result }
     end
   end
 end
+
+# require 'net/http'
+
+# urls = [
+#   {'link' => 'http://www.google.com/'},
+#   {'link' => 'http://www.facebook.com/'},
+#   {'link' => 'http://www.yahoo.com/'}
+# ]
+
+# urls.each do |u|
+#   Thread.new do
+#     u['content'] = Net::HTTP.get( URI.parse(u['link']) )
+#     puts "Successfully requested #{u['link']}"
+
+#     if urls.all? {|u| u.has_key?("content") }
+#       puts "Fetched all urls!"
+#       exit
+#     end
+#   end
+# end
