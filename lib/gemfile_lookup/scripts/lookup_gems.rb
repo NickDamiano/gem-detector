50b3c738697722a7dcca98d6256b8f5e12c16206
Nick Damiano <damiano.nick@gmail.com>
2015-02-21 23:06:13 -0600

    removes example code for threads

--- lib/gemfile_lookup/scripts/lookup_gems.rb
+++ lib/gemfile_lookup/scripts/lookup_gems.rb
@@ -27,23 +27,3 @@ module GemfileLookup
     end
   end
 end
-#
-# require 'net/http'
-
-# urls = [
-#   {'link' => 'http://www.google.com/'},
-#   {'link' => 'http://www.facebook.com/'},
-#   {'link' => 'http://www.yahoo.com/'}
-# ]
-
-# urls.each do |u|
-#   Thread.new do
-#     u['content'] = Net::HTTP.get( URI.parse(u['link']) )
-#     puts "Successfully requested #{u['link']}"
-
-#     if urls.all? {|u| u.has_key?("content") }
-#       puts "Fetched all urls!"
-#       exit
-#     end
-#   end
-# end
