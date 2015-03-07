6717b750ddd48f8ba9c227818c967d5f74211801
Nick Damiano <damiano.nick@gmail.com>
2015-03-07 10:45:31 -0600

    closes issue #8 by changing parsing so it parses out the gem name using regex and single or double quotes.

--- lib/gemfile_lookup/scripts/parse_gems.rb
+++ lib/gemfile_lookup/scripts/parse_gems.rb
@@ -6,13 +6,13 @@ module GemfileLookup
       parsed_file = source_file.gsub(/\r/, '').split(/\n/)
       parsed_file.each do |line|
         if line.lstrip.start_with?('Gem ', 'gem ', 'GEM ')
-          if line =~ /\sif\s/
-            gem_and_version = line.lstrip[4..-1].split('if')
-          else
-            gem_and_version =  line.lstrip[4..-1].split(',')
-          end
+          # splits the gem name from anything that follows it
+          gem_and_version =  line.lstrip[4..-1].split(',')
+          gem_name = gem_and_version[0].split(/\'|\"/)[1]
+          p 'gem name is '
+          p gem_name
 
-          gem_name = gem_and_version[0]
+          # pushes the gem into the array of gems
           result.push(gem_name.to_s) unless gem_name.empty?
         end
 
@@ -21,12 +21,12 @@ module GemfileLookup
           #get rid of the # and then strip any white space. then start the index from 1
           line = line[1..-1].lstrip
           gem_and_version =  line[4..-1].split(',')
-          gem_name = gem_and_version[0][1...-1]
+          gem_name = gem_and_version[0].split(/\'|\"/)[1]
           commented_gems.push(gem_name.to_s) unless gem_name.empty?
         end
       end
 
-      result = result.map{ |gemname| gemname[1...-1] }
+      result = result.map{ |gemname| gemname }
 
       if result.size == 0
         return { success?: false, gem_list: [], commented_gems: [] }
