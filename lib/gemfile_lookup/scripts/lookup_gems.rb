89ec6f4a134e5c60445d4a8da705cddadf4b372d
Nick Damiano <damiano.nick@gmail.com>
2016-07-21 23:24:51 +0300

    Finishes code that retries api call is 429 error code (too many retries) is returned)

--- lib/gemfile_lookup/scripts/lookup_gems.rb
+++ lib/gemfile_lookup/scripts/lookup_gems.rb
@@ -1,6 +1,7 @@
 require 'open-uri'
 require 'json'
 require "resolv-replace.rb"
+require 'pry-byebug'
 
 #gets an array of hashes with gem name and version number or possibly just a sub array. 
 #returns an array of hashes with all of the different information about each gem
@@ -10,32 +11,24 @@ module GemfileLookup
     def self.run(gem_list)
       api_result = []
       thread_list = []
-      count = 0 # for api requests
+      gem_info = 429
       gem_info_parsed = nil
       puts "GEM_LIST ISSSS   #{gem_list}"
       gem_list.each do |gem_name|
-        sleep(0.05)
         thread_list << Thread.new do 
           # call the method to get the gem data
             # in that method, if the result is 429 have it return 429, otherwise if error have it put not found
-            # here if the error is 429, going into a loop calling the method until the error is no longer 429 or time out
-            
-          begin
-
-            gem_info = open("https://rubygems.org/api/v1/gems/#{gem_name}.json").read
-            puts "gem info is: #{gem_info}"
-            rescue OpenURI::HTTPError => the_error
-              puts "ERROR ERROR ERROR ERROR"
-              puts "#{the_error}"
-              if the_error == 429
-                call_api(gem_name)
-              end
+            # here if the error is 429, going into a loop calling the method until the error is no longer 429 or time
+            while(gem_info == 429)
+              gem_info = call_api(gem_name)
+            end
+            # if it's an integer it's an error code, gem wasn't found
+            if gem_info.is_a? String 
+              gem_info_parsed = JSON.parse(gem_info) unless gem_info.nil?
+              api_result.push(gem_info_parsed) unless gem_info_parsed.nil?
+            else
               api_result.push({"name" => gem_name, "info" => "Gem not found at rubygems.org"})
-              api_result
-          end
-            gem_info_parsed = JSON.parse(gem_info) unless gem_info.nil?
-            api_result.push(gem_info_parsed) unless gem_info_parsed.nil?
-
+            end
         end
       end
       thread_list.each { |thread| thread.join }
@@ -51,7 +44,13 @@ module GemfileLookup
     end
 
     def self.call_api(gem_name)
-
+      begin
+        gem_info = open("https://rubygems.org/api/v1/gems/#{gem_name}.json").read
+        puts "gem info is: #{gem_info}"
+      rescue OpenURI::HTTPError => e 
+        return Integer(e.io.status[0])
+      end
+      gem_info
     end
   end
 end
