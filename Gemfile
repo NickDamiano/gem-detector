28425b9d7b8c765af7da69ad8fc6426408b98cd5
Nick Damiano <damiano.nick@gmail.com>
2016-07-23 15:35:42 +0300

    adds a few comments and comments out resolv replace to see what happens on heroku

--- Gemfile
+++ Gemfile
@@ -2,9 +2,8 @@ source 'https://rubygems.org/'
 
 gem 'sinatra', '~>1.4.7'
 
-
-
 group :development do 
   gem 'sinatra-contrib', require: 'sinatra/reloader'
   gem 'pry-byebug'
 end
+
--- lib/gemfile_lookup/scripts/check_github.rb
+++ lib/gemfile_lookup/scripts/check_github.rb
@@ -1,6 +1,6 @@
 require 'open-uri'
 require 'json'
-require "resolv-replace.rb"
+# require "resolv-replace.rb"
 
 
 # GithubCheck looks for a line containing the github repo address, if it finds it, it grabs the text from 
@@ -9,7 +9,7 @@ module GemfileLookup
   class GithubCheck
     def self.run(original_text)
       text_size = original_text.split("\n").size
-
+      # If the 
       if original_text =~ /github/ && text_size == 1
         # splits https://github.com/username/project-title to
         #   ["https:", "", "github.com", "username", "project-title"]
