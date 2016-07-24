a2adb0532435b38fa1f6a75c4b7d3bb1e30f2c39
Nick Damiano <damiano.nick@gmail.com>
2016-07-24 17:21:16 +0300

    removes require pry-byebug from top of file

--- lib/gemfile_lookup/scripts/lookup_gems.rb
+++ lib/gemfile_lookup/scripts/lookup_gems.rb
@@ -1,7 +1,6 @@
 require 'open-uri'
 require 'json'
 require "resolv-replace.rb"
-require 'pry-byebug'
 
 #gets an array of hashes with gem name and version number or possibly just a sub array. 
 #returns an array of hashes with all of the different information about each gem
